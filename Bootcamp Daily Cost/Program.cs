410331d4a6f942df6fd709437785af4ec1aada54
Seth-Race <sethrace@gmail.com>
2021-09-24 09:59:27 -0400

    Add project files.

---
+++ Bootcamp Daily Cost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bootcamp_Daily_Cost {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("To the C# World...");
+
+
+            var TotalCost = 15500m;
+            var AvgDailyCost = TotalCost / 65;
+            Console.WriteLine($"Total Cost is {TotalCost}, Avg is {AvgDailyCost}");
+
+
+
+        }
+        
+       }
+}
