5a04015387275e1db6a8e5b3730c2c67401e3b2e
Seth-Race <sethrace@gmail.com>
2021-09-24 10:32:17 -0400

    Added.. to file

--- Bootcamp Daily Cost/Program.cs
+++ Bootcamp Daily Cost/Program.cs
@@ -10,7 +10,16 @@ namespace Bootcamp_Daily_Cost {
 
             var TotalCost = 15500m;
             var AvgDailyCost = TotalCost / 65;
+            var PreMessage = "Total cost is ";
+            var PostMessage = "Daily Cost";
+            var Message = "{PreMessage}{AvgDailyCost}{PostMessage}";
+            Console.WriteLine($"{PreMessage}{AvgDailyCost}{PostMessage}");
             Console.WriteLine($"Total Cost is {TotalCost}, Avg is {AvgDailyCost}");
+            Console.WriteLine($"{Message}");
+            System.Diagnostics.Debug.WriteLine($"{Message}");
+            
+
+
 
 
 
